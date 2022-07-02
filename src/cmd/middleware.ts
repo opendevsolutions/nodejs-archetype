f5040c68aec0f3026213704efed5abb865224d05
maurogalindez90 <maurogalindez90@github.com>
2022-07-02 19:46:33 -0300

    cors

--- src/cmd/middleware.ts
+++ src/cmd/middleware.ts
@@ -1,7 +1,8 @@
 import express = require('express');
 import bodyParser = require('body-parser');
+var cors = require('cors');
 
 export default (app: express.Application) => {
     app.use(bodyParser.json({limit: "50mb"}));
-    // sumar todo middleware que se quiera utilizar
+    app.use(cors());
 }
